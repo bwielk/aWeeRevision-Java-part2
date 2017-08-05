93e1119dda0381356dca3776b308dd19c9e0dd2d
bwielk <bwielk@gmail.com>
2017-08-05 12:05:28 +0200

    added capitalisation

--- src/controlFlows/Main.java
+++ src/controlFlows/Main.java
@@ -5,6 +5,11 @@ public class Main {
 		switchMethod('A');
 		switchMethod('b');
 		switchMethod('X');
+		System.out.println(".....................................");
+		validName("ALeX");
+		validName("aNN");
+		validName("Stephen");
+		System.out.println(".....................................");
 	};
 	
 	public static void switchMethod(char value){
@@ -15,6 +20,18 @@ public class Main {
 			break;
 		default:
 			System.out.println("Not found");
+			break;
+		};
+	};
+	
+	public static void validName(String name){
+		String validName = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();//capitalisation
+		switch(validName){
+		case "Ann": case "Alice": case "Arthur": case "Kim": case "Alex":
+			System.out.println("Welcome " + validName + "!");
+			break;
+		default:
+			System.out.println("Sorry. You have no access.");
 			break;
 		};
 	};
